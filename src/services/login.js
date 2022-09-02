94215b369cb9078a16d0f752612164b932fbdb67
diego axsel garcia <diego.garciasi@autonoma.edu.co>
2022-09-01 20:13:03 -0500

    feat:realizar parte de login

--- src/services/login.js
+++ src/services/login.js
@@ -1,27 +1,8 @@
 import axios from 'axios'
-import Cookies from 'universal-cookie'
 
 const conexionDb = async(db) => {
-
-    const cookies = new Cookies();
-    let url = 'http://localhost:3001/users'
-
-    await axios.get(url, {params:{user: db.user, password: db.password}})
-    .then(response =>{
-        return response.data;
-    })
-    .then(response=>{
-        if (response.length===1) {
-            var respuesta=response[0];
-            cookies.set('id', respuesta.id, {path:'/'});
-            cookies.set('user', respuesta.user, {path:'/'});
-            cookies.set('name', respuesta.name, {path:'/'});
-            window.location.href="./home";
-        }else{
-            alert('El usuario o la contraseña no son correctos')
-        }
-    })
-    .catch(error =>{console.log(error)})
+    axios.post("http://3.141.169.163:3000/api/login/oauth",   { "email"  : "diego@gmail.com",
+    "passwd" : "1234567"}).console.log( response => console.log(response))
 }
 
 export default conexionDb
