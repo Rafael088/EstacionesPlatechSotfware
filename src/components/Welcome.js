4a554d29e2589d8c739e07ab19cc29499c728f16
Rafael088 <jerafa088@gmail.com>
2022-09-02 23:01:27 -0500

    fix: diseño

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,14 +1,17 @@
+
 import '../css/welcome.css';
+import Cards from './chirldrens/cards';
 
 function Welcome() {
     return ( 
         <div className="contWelcome">
             <div className='contWelHead'>
                 <h3>Guia del software</h3>
-                
             </div>
             <div className='contWelBody'>
-                
+                <Cards/>
+                <Cards/>
+                <Cards/>
             </div>
         </div>
      );
---
+++ src/components/chirldrens/cards.js
@@ -0,0 +1,9 @@
+function Cards() {
+    return ( 
+        <div className='contCard'>
+                    
+        </div>
+     );
+}
+
+export default Cards;
