4c23b0519b5f28901a4efcdf2fd3e86b17ae6788
diego axsel garcia <diego.garciasi@autonoma.edu.co>
2022-09-03 23:38:56 -0500

    fix: agregar mapa

--- src/app.js
+++ src/app.js
@@ -23,26 +23,37 @@ function App() {
     }
   ]
 
-  return (
-    <>
-        <Routes>
-          <Route path='/' element={<Login/>}/>
-          <Route path='/register' element={<Register/>}/>
-        </Routes>
+  const path = window.location.pathname
 
-        <div className="contHome">
+  const pathPermit = ["/", "/register"]
+
+  const EnterApp = () => {
+    return <>
+      <div className="contHome">
           <div className='contPublic'>
             <Navbar buttons={buttons}/>
               <Routes>
-                  <Route path='/home/*' element={<Home/>}/>
+                  <Route path='/home' element={<Home/>}/>
               </Routes>
               <div className='contBody'>
               <Routes>
-                  {buttons.map((v) => <Route path = {v.path} element = {v.component}/>)}
+                  {buttons.map((v, i) => <Route path = {v.path} element = {v.component} key = {i}/>)}
               </Routes>
               </div>
           </div>
         </div>
+    </>
+  }
+
+  return (
+    <>
+        <Routes>
+          <Route exact path='/' element={<Login/>}/>
+          <Route exact path='/register' element={<Register/>}/>
+        </Routes>
+
+      {pathPermit.includes(path) ? <></> : EnterApp()}
+
     </>
   );
 }
